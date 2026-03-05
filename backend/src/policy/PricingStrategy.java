ea2564cad4c25c06704b6255eb49e30cea642956
jingchun-ji <jingchun@my.yorku.ca>
2026-03-05 02:52:01 -0500

    Update PricingStrategy.java

--- backend/src/policy/PricingStrategy.java
+++ backend/src/policy/PricingStrategy.java
@@ -4,5 +4,5 @@ import bookingstates.Booking;
 import service.Service;
 
 public interface PricingStrategy {
-	double calculatePrice(Service service,Booking booking);
+	double calculatePrice(Service service, Booking booking);
 }
