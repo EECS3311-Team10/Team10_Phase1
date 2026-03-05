b85bb6fad888f5fc977ab5e9142a628cd2c9dacf
jingchun-ji <jingchun@my.yorku.ca>
2026-03-05 05:18:45 -0500

    Update PaidState.java

--- backend/src/bookingstates/PaidState.java
+++ backend/src/bookingstates/PaidState.java
@@ -1,5 +1,13 @@
 package bookingstates;
 
 public class PaidState extends BaseBookingState {
+    @Override
+    public void handleCancel(Booking booking) {
+        booking.setState(new CancelledState());
+    }
 
+    @Override
+    public void handleComplete(Booking booking) {
+        booking.setState(new CompletedState());
+    }
 }
