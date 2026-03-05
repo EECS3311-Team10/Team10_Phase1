c449e36eaff9967436a6f046951249b17c682b9c
jingchun-ji <jingchun@my.yorku.ca>
2026-03-05 05:18:51 -0500

    Update PendingPaymentState.java

--- backend/src/bookingstates/PendingPaymentState.java
+++ backend/src/bookingstates/PendingPaymentState.java
@@ -1,5 +1,12 @@
 package bookingstates;
 
 public class PendingPaymentState extends BaseBookingState {
-
+    @Override
+    public void handleCancel(Booking booking) {
+        booking.setState(new CancelledState());
+    }
+    @Override
+    public void handlePayment(Booking booking) {
+        booking.setState(new PaidState());
+    }
 }
