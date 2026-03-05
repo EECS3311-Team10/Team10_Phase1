3877cef09ee61443183a8141c47bc07116375c72
Khiem (Kae) Nguyen <tknguyen1301@gmail.com>
2026-03-05 15:00:26 -0500

    Update Client.java
    Added credit card payment method add

--- backend/src/Users/Client.java
+++ backend/src/Users/Client.java
@@ -1,19 +1,21 @@
 package Users;
+
 import bookingstates.*;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 import payment.*;
 
 public class Client extends User {
 
-    private static int idCounter = 1; 
-    
+    private static int idCounter = 1;
+
     private List<PaymentMethod> paymentMethods;
     private List<Booking> bookings;
     private List<Payment> paymentHistory;
 
     public Client(String name, String email, String phone) {
-        super(name, email, phone, "Client"); 
+        super(name, email, phone, "Client");
         this.setRole("Client");
         this.paymentMethods = new ArrayList<>();
         this.bookings = new ArrayList<>();
@@ -41,15 +43,26 @@ public class Client extends User {
         return this.paymentMethods;
     }
 
-    public void addPaymentMethod(PaymentMethod payMethod){
-        this.paymentMethods.add(payMethod);
-
+    // methods for adding paymentMethods
+    // credit card
+    public boolean addPaymentMethod(String cardNumber, LocalDate expiryDate,
+            String cvv, String cardHolderName) {
+        CreditCard creditCard = new CreditCard(cardNumber, cardHolderName,
+                expiryDate, cvv);
+        if (!creditCard.validate()) {
+            return false;
+        }
+        paymentMethods.add(creditCard);
+        System.out.println("Your new payment method was added.");
+        // notification
+        return true;
     }
-    public void addPayment(Payment payment){
+
+    public void addPayment(Payment payment) {
         this.paymentHistory.add(payment);
     }
 
     public List<?> viewPaymentHistory() {
-        return this.paymentHistory; 
+        return this.paymentHistory;
     }
 }
