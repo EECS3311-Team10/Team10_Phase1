a632c289cc08201cef09de724130b450f83a3dff
jingchun-ji <jingchun@my.yorku.ca>
2026-03-05 02:51:55 -0500

    Update DiscountStrategy.java

--- backend/src/policy/DiscountStrategy.java
+++ backend/src/policy/DiscountStrategy.java
@@ -4,6 +4,7 @@ import bookingstates.Booking;
 import service.Service;
 
 public class DiscountStrategy implements PricingStrategy{
+    // Discount strategy: applies a fixed discount rate to the base price of the service
     private final double discountRate = 0.10;
 
     @Override
