eb5939261b431c20ebc2c6b8eb535b911bcf1d11
jingchun-ji <jingchun@my.yorku.ca>
2026-03-05 02:52:03 -0500

    Update StandardPricingPolicy.java

--- backend/src/policy/StandardPricingPolicy.java
+++ backend/src/policy/StandardPricingPolicy.java
@@ -4,8 +4,9 @@ import bookingstates.Booking;
 import service.Service;
 
 public class StandardPricingPolicy implements PricingStrategy{
-
-	public double calculatePrice(Service service,Booking booking) {
+	// Standard pricing policy: returns the base price of the service without any discounts or adjustments
+	@Override
+	public double calculatePrice(Service service, Booking booking) {
 		return service.getPrice();
 	}
 }
