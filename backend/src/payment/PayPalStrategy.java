bdccbeb7338d056a7e2ff6d46ee88ab7f09668ad
AronJoseph-21 <ajo21@my.yorku.ca>
2026-03-05 13:55:29 -0500

    FileName Fix

---
+++ backend/src/payment/PayPalStrategy.java
@@ -0,0 +1,27 @@
+package payment;
+
+public class PayPalStrategy implements PaymentStrategy {
+    private PayPal payPal;
+    
+    public PayPalStrategy(PayPal payPal) {
+        this.payPal = payPal;
+    }
+    
+    @Override
+    public void pay() {
+        System.out.println("Processing payment through PayPal for " + payPal.getPaymentDetails());
+    }
+
+    @Override
+    public PaymentMethod getPaymentMethod() {
+        return payPal;
+    }
+
+    @Override
+    public void simPaymentProcessing() {
+        // Simulate payment processing for PayPal
+        System.out.println("Simulating PayPal payment processing...");
+    }
+
+}
+
