2678540bb27cea1de16f7d02c3c84ef1f0f7b7ec
jingchun-ji <jingchun@my.yorku.ca>
2026-03-05 02:51:51 -0500

    Update DebitCardStrategy.java

--- backend/src/payment/DebitCardStrategy.java
+++ backend/src/payment/DebitCardStrategy.java
@@ -1,24 +1,19 @@
 package payment;
 
 public class DebitCardStrategy implements PaymentStrategy {
-    private final int cardNumber;
-    private String cardHolderName;
-    private String expiryDate;
-    public DebitCardStrategy(int cardNumber, String cardHolderName, String expiryDate) {
-        this.cardNumber = cardNumber;
-        this.cardHolderName = cardHolderName;
-        this.expiryDate = expiryDate;
+    private DebitCard debitCard;
+    public DebitCardStrategy(DebitCard debitCard) {
+        this.debitCard = debitCard;
     }
     
     @Override
     public void pay() {
-        // Implement payment logic for debit card
         System.out.println("Processing debit card payment...");
     }
     
     @Override
     public PaymentMethod getPaymentMethod() {
-        return null;
+        return debitCard;
     }
     @Override
     public void simPaymentProcessing() {
